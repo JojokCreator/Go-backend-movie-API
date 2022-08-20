949d98442ec1b41112449b45bc963770e2ba7302
Joe Klakus <joeyk2k@hotmail.com>
2022-08-20 10:30:37 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
+type Movie struct {
+	Title    string `json:"Title"`
+	Year     string `json:"Year"`
+	Rated    string `json:"Rated"`
+	Released string `json:"Released"`
+	Runtime  string `json:"Runtime"`
+	Genre    string `json:"Genre"`
+	Director string `json:"Director"`
+	Writer   string `json:"Writer"`
+	Actors   string `json:"Actors"`
+	Plot     string `json:"Plot"`
+	Language string `json:"Language"`
+	Country  string `json:"Country"`
+	Awards   string `json:"Awards"`
+	Poster   string `json:"Poster"`
+	// Ratings
+	Metascore  string `json:"Metascore"`
+	ImdbRating string `json:"imdbRating"`
+	ImdbVotes  string `json:"imdbVotes"`
+	ImdbId     string `json:"imdbId"`
+	Type       string `json:"Type"`
+	BoxOffice  string `json:"boxOffice"`
+	Production string `json:"production"`
+	Website    string `json:"website"`
+	Response   string `json:"Response"`
+	Error      string `json:"Error"`
+}
+
+type Search struct {
+	Search []Movie `json:"search"`
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/movies", getMovies)
+	router.GET("/movie", getMovie)
+	router.Run("localhost:8080")
+
+}
+func getMovies(c *gin.Context) {
+	search, ok := c.GetQuery("search")
+
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing search query parameter."})
+		return
+	}
+
+	dotenv := goDotEnvVariable("API_KEY")
+
+	response, err := http.Get(fmt.Sprintf("https://www.omdbapi.com/?apikey=%s&s=%s", dotenv, search))
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var responseObject Search
+	json.Unmarshal(responseData, &responseObject)
+
+	c.JSON(http.StatusOK, responseObject.Search)
+}
+
+func getMovie(c *gin.Context) {
+	search, ok := c.GetQuery("search")
+
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing search query parameter."})
+		return
+	}
+
+	dotenv := goDotEnvVariable("API_KEY")
+
+	response, err := http.Get(fmt.Sprintf("https://www.omdbapi.com/?apikey=%s=%s", dotenv, search))
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var responseObject Movie
+	json.Unmarshal(responseData, &responseObject)
+
+	c.JSON(http.StatusOK, responseObject)
+}
